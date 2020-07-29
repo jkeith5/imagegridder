50ecfda4551d75c9cb9146915573d72bb8a74127
jkeith5 <32349572+jkeith5@users.noreply.github.com>
2020-07-29 08:12:57 -0500

    hello

--- ImageGridder.py
+++ ImageGridder.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter import filedialog
 from PIL import Image
 
-class pinger(tk.Tk):
+class gridder(tk.Tk):
     def __init__(self):
         tk.Tk.__init__(self)
         self.initialize()
@@ -16,9 +16,6 @@ class pinger(tk.Tk):
         labelDisclaim.grid(column=2, row=1)
 
         # row 2
-        labelWidth = tk.Label(self, text='Enter the width of the grid, in pixels.')
-        labelWidth.grid(column=4,row=2)
-
         buttonOpen = tk.Button(self, text="Select an Image", command=self.openExplorer)
         buttonOpen.grid(column=2, row=2)
 
@@ -26,22 +23,36 @@ class pinger(tk.Tk):
         labelStaticImg= tk.Label(self, text="Width of image, in pixels: ")
         labelStaticImg.grid(column=1,row=3)
 
-        labelImgWidth = tk.Label(self, textvariable=self.iWidth)
+        labelImgWidth = tk.Label(self, textvariable=self.iWidth, anchor='w')
         labelImgWidth.grid(column=2,row=3)
 
         labelStaticHeight= tk.Label(self, text="Height of image, in pixels: ")
         labelStaticHeight.grid(column=3,row=3)
 
-        labelImgHeight = tk.Label(self, textvariable=self.iHeight)
+        labelImgHeight = tk.Label(self, textvariable=self.iHeight, anchor='w')
         labelImgHeight.grid(column=4,row=3)
 
+        # row 4
+        labelInstructions = tk.Label(self, text='First, select an image, then below enter, in pixels, how wide and tall you want each grid section to be. If left empty, it will try to square them. Default value will try to have a 10x10 grid.', anchor='w', wraplength=200)
+        labelInstructions.grid(column=2,row=4)
+
+        # row 5
+        labelWidth = tk.Label(self, text='Enter the width of the grid, in pixels: ')
+        labelWidth.grid(column=1,row=5)
+        
+        labelHeight = tk.Label(self, text='Enter the width of the grid, in pixels: ')
+        labelHeight.grid(column=3, row=5)
+
+        # row 8 non-grid image to gridded image, pics
+        
+
         # row 9
-        button = tk.Button(self,text="exit",command=self.closeProgram)
-        button.grid(column=3,row=9)
+        button = tk.Button(self,text="Exit",command=self.closeProgram)
+        button.grid(column=2,row=9)
 
         # row 10
-        labelSig = tk.Label(self, text='By Johnathan Keith, 2020. Ver 1.0')
-        labelSig.grid(column=3,row=10)
+        labelSig = tk.Label(self, text='By Johnathan Keith, 2020. Ver 1.0. This is free-to-use, and will always be. This was willingly distributed to the public.',wraplength=350)
+        labelSig.grid(column=2,row=10)
 
     def openFile(self, imagefilename):
         Img = Image.open(imagefilename)
@@ -59,7 +70,6 @@ class pinger(tk.Tk):
            self.openFile(filename)
 
 if __name__ == "__main__":
-    app = pinger()
+    app = gridder()
     app.title('Image Gridder')
-    app.minsize(height=680,width=480)
     app.mainloop()
